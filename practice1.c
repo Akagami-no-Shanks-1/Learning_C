e34711056fa40d6c31eff78f2e9433b9be67103d
Red <gamerhyper190@gmail.com>
2026-05-09 20:13:01 +0530

    Added 1 practice problem : Area of Rect

---
+++ practice1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// This is practice problem 1
+int main()
+{
+    int l, b;
+    printf("Enter length :");
+    scanf("%d", &l);
+    printf("Enter breadth :");
+    scanf("%d", &b);
+    printf("Area of rectangle is %d", l * b);
+    return 0;
+}
