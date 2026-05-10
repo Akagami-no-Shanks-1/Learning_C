06b1997aa445f2fd403123762783904ce1350965
Red <gamerhyper190@gmail.com>
2026-05-10 12:38:07 +0530

    Refactor practice1.c to improve shape selection logic and add 3D shape calculations (Raw version)

--- practice1.c
+++ practice1.c
@@ -3,56 +3,97 @@
 // This is practice problem 1 modified
 int main()
 {
-    int shape, radius, length, width, base, height; // This is the variable declaration section
+    while (1)
+    {
+        int shape, subshape, radius, length, width, base, height; // This is the variable declaration section
 
-    printf("Chose shape to find area/volume of:\n");
-    printf("Circle: 1\n Rectangle: 2\n Square: 3\n Switch to 3D: 4\n");
-    scanf("%d", &shape); // This takes input from user to chose the shape
+        printf("=====2D Shapes=====\n");
+        printf("Chose shape to find area/volume of or 0 to quit:\n");
+        printf("Circle: 1\n Rectangle: 2\n Square: 3\n Switch to 3D: 4\n");
+        scanf("%d", &shape); // This takes input from user to chose the shape
 
-    if (shape == 1)
-    {
-        printf("Enter the radius of the circle:"); // This takes input from user for radius of circle
-        scanf("%d", &radius);
-        printf("Area of Circle = %.2f", 3.14 * radius * radius); // This calculates area of circle pi*r^2
-    }
-    else if (shape == 2)
-    {
-        printf("Enter length of the rectangle:"); // This takes input for the length of rectangle
-        scanf("%d", &length);
-        printf("Enter width of the rectangle:"); // This takes input for the width of rectangle
-        scanf("%d", &width);
-        printf("Area of Rectangle = %d", length * width); // This calculates area of rectangle length*width
-    }
-    else if (shape == 3)
-    {
-        printf("Enter the side of the Square:"); // This takes input for the side of square
-        scanf("%d", &length);
-        printf("Area of Square = %d", length * length); // This calculates area of square side^2
-    }
-    else if (shape == 4)
-    {
-        printf("Cylinder: 11\n Cone: 12\n Sphere: 13\n Cuboid: 21\n Cube: 31\n");
-        scanf("%d", &shape);
-        if (shape == 11)
+        if (shape == 0)
         {
-            printf("Enter the radius of the cylinder:"); // This takes input for the radius of cylinder
-            scanf("%d", &radius);
-            printf("Enter the height of the cylinder:"); // This takes input for the height of cylinder
-            scanf("%d", &height);
-            printf("Volume of the cylinder = %.2f\n", 3.14 * radius * radius * height);             // This calculates volume of cylinder pi*r^2*h
-            printf("Surface area of the cylinder = %.2f\n", 2 * 3.14 * radius * (radius + height)); // This calculates surface area of cylinder 2*pi*r*(r+h)
+            break; // This breaks the loop if user chooses to quit
         }
-        else if (shape == 12)
+
+        switch (shape)
         {
-            printf("Enter the radius of the cone:"); // This takes input for the radius of cone
+        case 1:
+            printf("Enter the radius of the circle:"); // This takes input from user for radius of circle
             scanf("%d", &radius);
-            printf("Enter the height of the cone:"); // This takes input for the height of cone
-            scanf("%d", &height);
-            printf("Volume of the cone = %.2f\n", (3.14 * radius * radius * height) / 3); // This calculates volume of cone (pi*r^2*h)/3
-            printf("Surface area of the cone = %.2f\n", 3.14 * radius * (radius + sqrt(height * height + radius * radius)));
-            // This calculates surface area of cone pi*r*(r+l) where l is the slant height calculated using Pythagorean theorem
+            printf("Area of Circle = %.2f", 3.14 * pow(radius, 2)); // This calculates area of circle pi*r^2
+            break;
+
+        case 2:
+            printf("Enter length of the rectangle:"); // This takes input for the length of rectangle
+            scanf("%d", &length);
+            printf("Enter width of the rectangle:"); // This takes input for the width of rectangle
+            scanf("%d", &width);
+            printf("Area of Rectangle = %d", length * width); // This calculates area of rectangle length*width
+            break;
+
+        case 3:
+            printf("Enter the side of the Square:"); // This takes input for the side of square
+            scanf("%d", &length);
+            printf("Area of Square = %d", length * length); // This calculates area of square side^2
+            break;
+
+        case 4:
+            printf("=====3D Shapes=====\n");
+            printf("Chose 3D shape for volume and Surface area:\n");
+            printf("Sphere: 1\n Cylinder: 2\n Cone: 3\n Cuboid: 4\n Cube: 5\n");
+            scanf("%d", &subshape); // This takes input for the 3D shape
+
+            switch (subshape)
+            {
+            case 1:
+                printf("Enter the radius of the sphere:"); // This takes input for the radius of sphere
+                scanf("%d", &radius);
+                printf("Volume of Sphere = %.2f\n", (4.0 / 3.0) * 3.14 * pow(radius, 3)); // This calculates volume of sphere (4/3)*pi*r^3
+                printf("Surface Area of Sphere = %.2f", 4.0 * 3.14 * pow(radius, 2));     // This calculates surface area of sphere 4*pi*r^2
+                break;
+            case 2:
+                printf("Enter the radius of the cylinder:"); // This takes input for the radius of cylinder
+                scanf("%d", &radius);
+                printf("Enter the height of the cylinder:"); // This takes input for the height of cylinder
+                scanf("%d", &height);
+                printf("Volume of Cylinder = %.2f\n", 3.14 * pow(radius, 2) * height);              // This calculates volume of cylinder pi*r^2*h
+                printf("Surface Area of Cylinder = %.2f", 2.0 * 3.14 * radius * (radius + height)); // This calculates surface area of cylinder 2*pi*r*(r+h)
+                break;
+            case 3:
+                printf("Enter the radius of the cone:"); // This takes input for the radius of cone
+                scanf("%d", &radius);
+                printf("Enter the height of the cone:"); // This takes input for the height of cone
+                scanf("%d", &height);
+                printf("Volume of Cone = %.2f\n", (1.0 / 3.0) * 3.14 * pow(radius, 2) * height);                         // This calculates volume of cone (1/3)*pi*r^2*h
+                printf("Surface Area of Cone = %.2f", 3.14 * radius * (radius + sqrt(pow(height, 2) + pow(radius, 2)))); // This calculates surface area of cone pi*r*(r+l) where l is slant height
+                break;
+            case 4:
+                printf("Enter the length of the cuboid:"); // This takes input for the length of cuboid
+                scanf("%d", &length);
+                printf("Enter the width of the cuboid:"); // This takes input for the width of cuboid
+                scanf("%d", &width);
+                printf("Enter the height of the cuboid:"); // This takes input for the height of cuboid
+                scanf("%d", &height);
+                printf("Volume of Cuboid = %d\n", length * width * height);                                     // This calculates volume of cuboid length*width*height
+                printf("Surface Area of Cuboid = %d", 2 * (length * width + width * height + length * height)); // This calculates surface area of cuboid 2*(lw + wh + lh)
+                break;
+            case 5:
+                printf("Enter the side of the cube:"); // This takes input for the side of cube
+                scanf("%d", &length);
+                printf("Volume of Cube = %d\n", pow(length, 3));         // This calculates volume of cube side^3
+                printf("Surface Area of Cube = %d", 6 * pow(length, 2)); // This calculates surface area of cube 6*side^2
+                break;
+            default:
+                printf("Invalid 3D shape selection. Please try again.\n");
+                break;
+            }
+            break;
+        default:
+            printf("Invalid shape selection. Please try again.\n");
+            break;
         }
-        printf("Rest coming soon...");
     }
     return 0;
 }
